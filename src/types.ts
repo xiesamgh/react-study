58f9dd5cd5544becad537f2fe0fb2750294a8ce7
Sam Xie <xiesamgm@gmail.com>
2026-01-15 10:43:25 +1300

    Create src/types.ts file with Student and StudentState interfaces

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export interface Student {
+  id: number;
+  name: string;
+  age: number;
+  grade: string;
+  email: string;
+}
+
+export interface StudentState {
+  students: Student[];
+  searchTerm: string;
+  filterGrade: string;
+}
